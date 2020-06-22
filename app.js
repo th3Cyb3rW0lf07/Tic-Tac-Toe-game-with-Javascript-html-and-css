3cf9e4a85b3dae7be5835a06fe613e03f8aa0b8d
Hamilton Junior <66115581+Bezalel27@users.noreply.github.com>
2020-06-22 15:31:31 +0100

    Update app.js

--- app.js
+++ app.js
@@ -55,7 +55,7 @@ function endGame(draw) {
     if (draw) {
         winningMessageTextElement.innerText = 'Draw!'
     } else {
-        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+        winningMessageTextElement.innerText = `${circleTurn ? "O's" : "X's"} Win!`
     }
     winningMessageElement.classList.add('show')
 }
